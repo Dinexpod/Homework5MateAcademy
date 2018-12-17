6496c2f6b89be2e854181b5e0dcb990e47286c2d
Анатолий <dinexpod@gmail.com>
2018-12-17 17:18:57 +0200

    test

--- src/main/java/shop/TradeShop.java
+++ src/main/java/shop/TradeShop.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-
+/
 public class TradeShop {
     private List<Fruit> fruits = new ArrayList<>();
     private final static long DAY_IN_MLS = 86_400_000;
